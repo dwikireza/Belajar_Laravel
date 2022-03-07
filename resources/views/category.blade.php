4b8f68d32c7a4661d17fc84b95b7c127982648a1
dwikireza <dwikireza08@gmail.com>
2022-03-07 10:57:12 +0700

    Post Category & Eloquent Relationship

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,15 +1,16 @@
 @extends('layout.main')
 
 @section('container')
-<H1>Halaman | Blog</H1>
-<article class="mb-5">
 
+<h1 class="mb-5">Post Category : {{ $category }}</h1>
     @foreach ($posts as $post)
+    <article>
     <h2>
         <a href="/posts/{{ $post->slug   }}">{{ $post->title }}</a>
     </h2>
     <p>{{ $post->excerpt }}</p>
-
+    </article>
     @endforeach
-</article>
+
 @endsection
+        
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,9 @@
 @section('container')
 
     <h2 class=""mb-5>{{ $post->title }}</h2>
+<p>By. Dwiki reza  in <a href="/categories/{{ $post->category->slug }}">
+    {{ $post->category->name }}</a>
+    </p>
     {!! $post->body !!}
 
 
