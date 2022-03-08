9d704aedf0ca5d43cedceda6c38221c290c0ee31
dwikireza <dwikireza08@gmail.com>
2022-03-08 12:07:53 +0700

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Foundation\Auth\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,19 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name'      => 'Dwiki Reza',
-            'email'    => 'dwiki@gmail.com',
-            'password'  => bcrypt('1245')
-        ]);
 
-        User::create([
-            'name'      => 'Reza',
-            'email'     => 'reza@gmail.com',
-            'password'  => bcrypt('1245')
-        ]);
+
+        // User::create([
+        //     'name'      => 'Dwiki Reza',
+        //     'email'    => 'dwiki@gmail.com',
+        //     'password'  => bcrypt('1245')
+        // ]);
+
+        // User::create([
+        //     'name'      => 'Reza',
+        //     'email'     => 'reza@gmail.com',
+        //     'password'  => bcrypt('1245')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name'  => 'Personal',
@@ -40,81 +44,8 @@ class DatabaseSeeder extends Seeder
             'slug'  => 'web-programming'
         ]);
 
-        Post::create([
-            'title'     => 'Judul Pertama',
-            'slug'      => 'judul-pertama',
-            'excerpt'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'body'      => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            ut error optio voluptate voluptatibus quae dolorum asperiores inventore
-            expedita, vero et odit maxime molestias nulla ab ipsa quis? Consequuntur
-            maiores doloremque eligendi commodi tempora possimus, distinctio eaque
-            recusandae magni ipsam asperiores laborum ratione totam exercitationem.
-            Quam, praesentium. Natus voluptates a odit! Impedit quae rem alias, culpa
-            labore ut ipsum blanditiis at beatae, ad tempore earum accusantium esse
-            dignissimos quo eveniet suscipit nam minus quidem sunt, dolore dicta natus
-            voluptatum consectetur. Cum sunt consectetur quam, expedita amet dolore ex
-            libero rem cumque id blanditiis possimus laudantium vel, nesciunt nihil
-            ipsum dolor!',
-            'category_id' => 1,
-            'user_id'   =>2
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title'     => 'Judul Ke Dua',
-            'slug'      => 'judul-kedua',
-            'excerpt'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'body'      => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            ut error optio voluptate voluptatibus quae dolorum asperiores inventore
-            expedita, vero et odit maxime molestias nulla ab ipsa quis? Consequuntur
-            maiores doloremque eligendi commodi tempora possimus, distinctio eaque
-            recusandae magni ipsam asperiores laborum ratione totam exercitationem.
-            Quam, praesentium. Natus voluptates a odit! Impedit quae rem alias, culpa
-            labore ut ipsum blanditiis at beatae, ad tempore earum accusantium esse
-            dignissimos quo eveniet suscipit nam minus quidem sunt, dolore dicta natus
-            voluptatum consectetur. Cum sunt consectetur quam, expedita amet dolore ex
-            libero rem cumque id blanditiis possimus laudantium vel, nesciunt nihil
-            ipsum dolor!',
-            'category_id' => 1,
-            'user_id'   =>1
-        ]);
-
-        Post::create([
-            'title'     => 'Judul Ke Tiga',
-            'slug'      => 'judul-ketiga',
-            'excerpt'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'body'      => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            ut error optio voluptate voluptatibus quae dolorum asperiores inventore
-            expedita, vero et odit maxime molestias nulla ab ipsa quis? Consequuntur
-            maiores doloremque eligendi commodi tempora possimus, distinctio eaque
-            recusandae magni ipsam asperiores laborum ratione totam exercitationem.
-            Quam, praesentium. Natus voluptates a odit! Impedit quae rem alias, culpa
-            labore ut ipsum blanditiis at beatae, ad tempore earum accusantium esse
-            dignissimos quo eveniet suscipit nam minus quidem sunt, dolore dicta natus
-            voluptatum consectetur. Cum sunt consectetur quam, expedita amet dolore ex
-            libero rem cumque id blanditiis possimus laudantium vel, nesciunt nihil
-            ipsum dolor!',
-            'category_id' => 2,
-            'user_id'   =>1
-        ]);
-
-        Post::create([
-            'title'     => 'Judul Ke Empat',
-            'slug'      => 'judul-keempat',
-            'excerpt'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'body'      => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            ut error optio voluptate voluptatibus quae dolorum asperiores inventore
-            expedita, vero et odit maxime molestias nulla ab ipsa quis? Consequuntur
-            maiores doloremque eligendi commodi tempora possimus, distinctio eaque
-            recusandae magni ipsam asperiores laborum ratione totam exercitationem.
-            Quam, praesentium. Natus voluptates a odit! Impedit quae rem alias, culpa
-            labore ut ipsum blanditiis at beatae, ad tempore earum accusantium esse
-            dignissimos quo eveniet suscipit nam minus quidem sunt, dolore dicta natus
-            voluptatum consectetur. Cum sunt consectetur quam, expedita amet dolore ex
-            libero rem cumque id blanditiis possimus laudantium vel, nesciunt nihil
-            ipsum dolor!',
-            'category_id' => 2,
-            'user_id'   =>2
-        ]);
     }
 }
 
